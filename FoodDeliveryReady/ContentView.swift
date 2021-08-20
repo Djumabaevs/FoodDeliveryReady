25a93e169a168c1ad81a1d10cc81ee9acfba052a
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-21 01:26:05 +0600

    Initial Commit

---
+++ FoodDeliveryReady/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodDeliveryReady
+//
+//  Created by Bakyt Dzhumabaev on 21/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
